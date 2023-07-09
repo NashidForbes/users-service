88d6a588da40f536d85809e224030c623e801126
NashidForbes <NashidForbes@biohumans.com>
2023-07-08 23:09:48 -0400

    Adding the rest of the user and user events handler functionality and dependencies.

--- src/main/java/io/axoniq/usersservice/query/handler/UserEventsHandler.java
+++ src/main/java/io/axoniq/usersservice/query/handler/UserEventsHandler.java
@@ -1,9 +1,31 @@
 package io.axoniq.usersservice.query.handler;
 
+import io.axoniq.core.model.PaymentDetails;
+import io.axoniq.core.model.User;
+import io.axoniq.core.query.FetchUserPaymentDetailsQuery;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserEventsHandler {
 
-    //public User getUserPaymentDetails
+    public User getUserPaymentDetails(FetchUserPaymentDetailsQuery query){
+
+        PaymentDetails paymentDetails = PaymentDetails.builder()
+                .cardNumber("123Card")
+                .cvv("123")
+                .name("NASH FORBES")
+                .validUntilMonth(12)
+                .validUntilYear(2030)
+                .build();
+
+        User userRest = User.builder()
+                .firstName("Sergey")
+                .lastName("Kargopolov")
+                .foodCartId(query.getFoodCartId())
+                .paymentDetails(paymentDetails)
+                .build();
+
+        return userRest;
+
+    }
 }
